b7dac2454dbad8b900d71438fa6e0500fd3dff51
Erismann <matteo.erismann@student.vinci.be>
2024-01-15 17:21:07 +0100

    function

---
+++ gamertag.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    checkGamerTag (tag) {
+      if (tag.length < 9) return '“Invalid - gamertag length must be at least 8 characters'
+  
+      return 'gamer tag ok'
+    }
+}
